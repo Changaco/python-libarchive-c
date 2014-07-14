697760556ab99997e7ec5d64386b3bd8c4c3dcf8
Changaco <changaco@changaco.oy.lc>
2014-07-14 18:31:45 +0200

    rewrite FFI declarations

--- libarchive/calls/archive_general.py
+++
@@ -1,8 +0,0 @@
-from ctypes import *
-
-from libarchive.library import libarchive
-
-c_archive_error_string = libarchive.archive_error_string
-c_archive_error_string.argtypes = [c_void_p]
-c_archive_error_string.restype = c_char_p
-
--- libarchive/exception.py
+++ libarchive/exception.py
@@ -1,2 +1,4 @@
 class ArchiveError(Exception):
-    pass
+    def __init__(self, msg, **kw):
+        Exception.__init__(self, msg)
+        self.__dict__.update(kw)
