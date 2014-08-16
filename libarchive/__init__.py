4ae57cea626149c3b2097fb5709c194a8fcaf895
Changaco <changaco@changaco.oy.lc>
2014-08-16 19:00:44 +0200

    expose custom_writer and memory_writer at the top

--- libarchive/__init__.py
+++ libarchive/__init__.py
@@ -7,12 +7,12 @@ from .entry import ArchiveEntry
 from .exception import ArchiveError
 from .extract import extract_file, extract_memory
 from .read import file_reader, memory_reader
-from .write import fd_writer, file_writer
+from .write import custom_writer, fd_writer, file_writer, memory_writer
 
 __all__ = [
     ArchiveEntry,
     ArchiveError,
     extract_file, extract_memory,
     file_reader, memory_reader,
-    fd_writer, file_writer,
+    custom_writer, fd_writer, file_writer, memory_writer
 ]
