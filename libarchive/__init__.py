be8f1832a8ed778a9945999718b0ed521f441312
Changaco <changaco@changaco.oy.lc>
2014-08-17 18:04:39 +0200

    expose fd_reader and extract_fd at the top

--- libarchive/__init__.py
+++ libarchive/__init__.py
@@ -5,14 +5,14 @@
 
 from .entry import ArchiveEntry
 from .exception import ArchiveError
-from .extract import extract_file, extract_memory
-from .read import file_reader, memory_reader
+from .extract import extract_fd, extract_file, extract_memory
+from .read import fd_reader, file_reader, memory_reader
 from .write import custom_writer, fd_writer, file_writer, memory_writer
 
 __all__ = [
     ArchiveEntry,
     ArchiveError,
-    extract_file, extract_memory,
-    file_reader, memory_reader,
+    extract_fd, extract_file, extract_memory,
+    fd_reader, file_reader, memory_reader,
     custom_writer, fd_writer, file_writer, memory_writer
 ]
