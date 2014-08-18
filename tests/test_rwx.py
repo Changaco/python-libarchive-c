678a4f7d4686830d420f3dcc84a2a2034f3a34c7
Changaco <changaco@changaco.oy.lc>
2014-08-18 11:51:44 +0200

    refactor tests

---
+++ tests/test_rwx.py
@@ -0,0 +1,81 @@
+# This file is part of a program licensed under the terms of the GNU Lesser
+# General Public License version 2 (or at your option any later version)
+# as published by the Free Software Foundation: http://www.gnu.org/licenses/
+
+"""Test reading, writing and extracting archives."""
+
+from __future__ import division, print_function, unicode_literals
+
+import libarchive
+from libarchive.extract import EXTRACT_OWNER, EXTRACT_PERM, EXTRACT_TIME
+
+from . import check_archive, in_dir, treestat
+
+
+def test_buffers(tmpdir):
+
+    # Collect information on what should be in the archive
+    tree = treestat('libarchive')
+
+    # Create an archive of our libarchive/ directory
+    buf = bytes(bytearray(1000000))
+    with libarchive.memory_writer(buf, 'gnutar', 'xz') as archive:
+        archive.add_files('libarchive/')
+
+    # Read the archive and check that the data is correct
+    with libarchive.memory_reader(buf) as archive:
+        check_archive(archive, tree)
+
+    # Extract the archive in tmpdir and check that the data is intact
+    with in_dir(tmpdir.strpath):
+        flags = EXTRACT_OWNER | EXTRACT_PERM | EXTRACT_TIME
+        libarchive.extract_memory(buf, flags)
+        tree2 = treestat('libarchive')
+        assert tree2 == tree
+
+
+def test_fd(tmpdir):
+    archive_file = open(tmpdir.strpath+'/test.tar.bz2', 'w+b')
+    fd = archive_file.fileno()
+
+    # Collect information on what should be in the archive
+    tree = treestat('libarchive')
+
+    # Create an archive of our libarchive/ directory
+    with libarchive.fd_writer(fd, 'v7tar', 'bzip2') as archive:
+        archive.add_files('libarchive/')
+
+    # Read the archive and check that the data is correct
+    archive_file.seek(0)
+    with libarchive.fd_reader(fd) as archive:
+        check_archive(archive, tree)
+
+    # Extract the archive in tmpdir and check that the data is intact
+    archive_file.seek(0)
+    with in_dir(tmpdir.strpath):
+        flags = EXTRACT_OWNER | EXTRACT_PERM | EXTRACT_TIME
+        libarchive.extract_fd(fd, flags)
+        tree2 = treestat('libarchive')
+        assert tree2 == tree
+
+
+def test_files(tmpdir):
+    archive_path = tmpdir.strpath+'/test.tar.gz'
+
+    # Collect information on what should be in the archive
+    tree = treestat('libarchive')
+
+    # Create an archive of our libarchive/ directory
+    with libarchive.file_writer(archive_path, 'ustar', 'gzip') as archive:
+        archive.add_files('libarchive/')
+
+    # Read the archive and check that the data is correct
+    with libarchive.file_reader(archive_path) as archive:
+        check_archive(archive, tree)
+
+    # Extract the archive in tmpdir and check that the data is intact
+    with in_dir(tmpdir.strpath):
+        flags = EXTRACT_OWNER | EXTRACT_PERM | EXTRACT_TIME
+        libarchive.extract_file(archive_path, flags)
+        tree2 = treestat('libarchive')
+        assert tree2 == tree
