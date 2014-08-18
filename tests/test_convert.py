b196ddf128def3b8b55be16c1634bc22c1ebba72
Changaco <changaco@changaco.oy.lc>
2014-08-18 16:37:36 +0200

    add a test for ArchiveWrite.add_entries()

---
+++ tests/test_convert.py
@@ -0,0 +1,31 @@
+# This file is part of a program licensed under the terms of the GNU Lesser
+# General Public License version 2 (or at your option any later version)
+# as published by the Free Software Foundation: http://www.gnu.org/licenses/
+
+
+from __future__ import division, print_function, unicode_literals
+
+from libarchive import memory_reader, memory_writer
+
+from . import check_archive, treestat
+
+
+def test_convert():
+
+    # Collect information on what should be in the archive
+    tree = treestat('libarchive')
+
+    # Create an archive of our libarchive/ directory
+    buf = bytes(bytearray(1000000))
+    with memory_writer(buf, 'gnutar', 'xz') as archive1:
+        archive1.add_files('libarchive/')
+
+    # Convert the archive to another format
+    buf2 = bytes(bytearray(1000000))
+    with memory_reader(buf) as archive1:
+        with memory_writer(buf2, 'zip') as archive2:
+            archive2.add_entries(archive1)
+
+    # Check the data
+    with memory_reader(buf2) as archive2:
+        check_archive(archive2, tree)
