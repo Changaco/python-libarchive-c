85830c877dd606f9e024736f25f3fabd0619b260
superr <superr@ddayweb.com>
2016-12-03 10:07:35 -0600

    fix ArchiveWrite.add_files() chokes on broken symlinks

--- libarchive/write.py
+++ libarchive/write.py
@@ -60,16 +60,17 @@ class ArchiveWrite(object):
                         entry.pathname = entry.pathname.lstrip('/')
                         read_disk_descend(read_p)
                         write_header(write_p, entry_p)
-                        try:
-                            with open(entry_sourcepath(entry_p), 'rb') as f:
-                                while 1:
-                                    data = f.read(block_size)
-                                    if not data:
-                                        break
-                                    write_data(write_p, data, len(data))
-                        except IOError as e:
-                            if e.errno != EISDIR:
-                                raise  # pragma: no cover
+                        if not entry.issym:
+                            try:
+                                with open(entry_sourcepath(entry_p), 'rb') as f:
+                                    while 1:
+                                        data = f.read(block_size)
+                                        if not data:
+                                            break
+                                        write_data(write_p, data, len(data))
+                            except IOError as e:
+                                if e.errno != EISDIR:
+                                    raise  # pragma: no cover
                         write_finish_entry(write_p)
                         entry_clear(entry_p)
 
