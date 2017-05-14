4784e43a9ca7835cdfbceba718a527e1f8033447
Kirill Isakov <ukwt@ya.ru>
2017-05-14 16:14:09 +0600

    Add missing libarchive flags, some security tests
    * Add missing flags supported by libarchive:
        - EXTRACT_SECURE_NOABSOLUTEPATHS;
        - EXTRACT_CLEAR_NOCHANGE_FFLAGS;
    
    * Add some tests for security options:
        - EXTRACT_SECURE_NODOTDOT;
        - EXTRACT_SECURE_NOABSOLUTEPATHS.

---
+++ tests/test_security_flags.py
@@ -0,0 +1,32 @@
+"""Test security-related extraction flags."""
+
+from __future__ import division, print_function, unicode_literals
+import pytest
+import os
+
+from libarchive import extract_file
+from libarchive.extract import EXTRACT_SECURE_NOABSOLUTEPATHS, \
+    EXTRACT_SECURE_NODOTDOT
+from libarchive.exception import ArchiveError
+from . import data_dir
+
+
+def run_test(flag, filename):
+    archive_path = os.path.join(data_dir, 'flags.tar')
+    try:
+        extract_file(archive_path)
+        with pytest.raises(ArchiveError):
+            extract_file(archive_path, flag)
+    finally:
+        if os.path.exists(filename):
+            os.remove(filename)
+
+
+def test_no_dot_dot():
+    run_test(EXTRACT_SECURE_NODOTDOT,
+             '../python-libarchive-c-test-dot-dot-file')
+
+
+def test_absolute():
+    run_test(EXTRACT_SECURE_NOABSOLUTEPATHS,
+             '/tmp/python-libarchive-c-test-absolute-file')
