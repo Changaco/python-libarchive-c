191577a935fe40ff83c08b0b604a42de2d695187
Changaco <changaco@changaco.oy.lc>
2018-06-10 15:15:04 +0200

    move `READDISK_*` constants to new `flags` module

--- libarchive/write.py
+++ libarchive/write.py
@@ -15,15 +15,6 @@ from .ffi import (
 )
 
 
-READDISK_RESTORE_ATIME = 0x0001
-READDISK_HONOR_NODUMP = 0x0002
-READDISK_MAC_COPYFILE = 0x0004
-READDISK_NO_TRAVERSE_MOUNTS = 0x0008
-READDISK_NO_XATTR = 0x0010
-READDISK_NO_ACL = 0x0020
-READDISK_NO_FFLAGS = 0x0040
-
-
 @contextmanager
 def new_archive_read_disk(path, flags=0, lookup=False):
     archive_p = read_disk_new()
