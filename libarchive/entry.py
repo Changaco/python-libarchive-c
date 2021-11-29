39af669adbc719ba2d79259c3382a75344c8d9f6
Changaco <changaco@changaco.oy.lc>
2021-11-29 11:33:35 +0100

    create the `ArchiveEntry.rdev` property

--- libarchive/entry.py
+++ libarchive/entry.py
@@ -275,6 +275,18 @@ class ArchiveEntry:
         ffi.entry_set_perm(self._entry_p, value)
 
     @property
+    def rdev(self):
+        return ffi.entry_rdev(self._entry_p)
+
+    @rdev.setter
+    def rdev(self, value):
+        if isinstance(value, tuple):
+            ffi.entry_set_rdevmajor(self._entry_p, value[0])
+            ffi.entry_set_rdevminor(self._entry_p, value[1])
+        else:
+            ffi.entry_set_rdev(self._entry_p, value)
+
+    @property
     def rdevmajor(self):
         return ffi.entry_rdevmajor(self._entry_p)
 
